31aee28f685be48311e3079ebc0556e376ecb9b8
Ashiqul Hoque Chowdhury <ashiqulhoque45@gmail.com>
2020-04-10 22:50:16 +0600

    implemented M1F2

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,6 +14,30 @@ class LoginController extends Controller
     }
 
     public function verify(Request $req){
-		return redirect('/login');
+		$validation = Validator::make($req->all(), [
+			'email'=>'required|email|exists:users',
+			'password'=>'required'
+		]);
+		if($validation->fails()){
+			return back()
+					->with('errors', $validation->errors())
+					->withInput();
+			return redirect()->route('login.index')
+							->with('errors', $validation->errors())
+							->withInput();		
+		}
+    	
+        $user = DB::table('users')
+                    ->where('email', $req->email)
+                    ->first();
+
+    	if($user != null){
+            $req->session()->put('email', $req->email);
+    		return view('welcome');
+
+    	}else{
+            $req->session()->flash('msg', 'try again');
+            return redirect('/login');
+		}
     }
 }
