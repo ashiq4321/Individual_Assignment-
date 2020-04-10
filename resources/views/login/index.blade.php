e7c77c395783ae0c11d9a185e6917e866e679a31
ashiq4321 <ashiqulhoque45@gmail.com>
2020-04-10 21:35:06 +0600

    implemented M1F1

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<h1>Login Page</h1>
+	<form method="post" >
+		@csrf
+<!-- 		{{csrf_field()}} -->		
+<!-- 		<input type="hidden" name="_token" value="{{csrf_token()}}"> -->
+		Email: <input type="text" name="email" > <br>
+		Password: <input type="password" name="password" ><br>
+		<input type="submit" name="submit" value="login" >
+	</form>
+	@foreach($errors->all() as $error)
+		{{$error}} <br>
+	@endforeach
+	<h3>{{session('msg')}}</h3>
+</body>
+</html>
