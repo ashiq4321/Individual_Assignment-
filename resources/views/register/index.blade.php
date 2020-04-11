b8f20b968abfaa1fd7d4d14bad45e9bc98b45e4c
Ashiqul Hoque Chowdhury <ashiqulhoque45@gmail.com>
2020-04-11 11:25:08 +0600

    implemented M3F2

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -7,7 +7,12 @@
 	<h1>register Page</h1>
 	<form method="post" >
 		@csrf
-
+		Name: <input type="text" name="uname" > <br>
+		Email: <input type="text" name="email" > <br>
+		Password: <input type="password" name="password" ><br>
+		Confirm Password: <input type="password" name="cpassword" ><br>
+		Company: <input type="text" name="ucompany" > <br>
+		<input type="submit" name="submit" value="Submit" >
 	</form>
 </body>
 </html>
